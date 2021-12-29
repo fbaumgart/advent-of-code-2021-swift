9390622f0069586e89ff5eb4283c52f2614d04c5
Filip Baumgart <filip.baumgart@rsqtechnologies.com>
2021-12-29 18:20:16 +0100

    Reorganize file structure

---
+++ AdventOfCode2021/AdventOfCode2021/AdventOfCode2021App.swift
@@ -0,0 +1,17 @@
+//
+//  AdventOfCode2021App.swift
+//  AdventOfCode2021
+//
+//  Created by Filip Baumgart on 29/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct AdventOfCode2021App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AdventOfCode2021/AdventOfCode2021/InputData.swift
@@ -0,0 +1,8 @@
+//
+//  InputData.swift
+//  AdventOfCode2021
+//
+//  Created by Filip Baumgart on 29/12/2021.
+//
+
+import Foundation
